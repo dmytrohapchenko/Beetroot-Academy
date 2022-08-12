6bd321cbb235c3f567dd057904700bfb0d103641
dmytrohapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2022-08-12 13:59:39 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,25 @@
+# TASK 1
+name = "Dmytro"
+day = "Friday"
+print("Good day", name + "!", day, "is a perfect day to learn some python.")
+
+# TASK 2
+name_first = "Dmytro"
+name_secod = "Hapchenko"
+name = name_first + ' ' + name_secod
+print("Good day", name + "!", day, "is a perfect day to learn some python.")
+
+# TASK 3
+from math import *
+
+a = 10
+b = 20
+print("Додавання =", a + b)
+print("Віднімання =", a - b)
+print("Ділення =", a / b)
+print("Множення =", a * b)
+print("Експонента а =", exp(a))
+print("Експонента b =", exp(b))
+print("Модуль а =", abs(a))
+print("Модуль b =", abs(b))
+print("Цілочисельне ділення =", a // b)
