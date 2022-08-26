0875a7c280feff9e5470b82387dde593d607fffb
dmytrohapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2022-08-27 00:24:21 +0300

    Add files via upload

---
+++ TASK 9/task9_1/module_average.py
@@ -0,0 +1,6 @@
+from module_summ import sum
+
+
+def average(a, b):
+
+    return sum(a, b) / 2
---
+++ TASK 9/task9_1/task9_1.py
@@ -0,0 +1,3 @@
+from module_average import average
+
+print('average:', average(int(input('first: ')), int(input('second: '))))
