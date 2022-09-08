ad6f21314173f79fb7a3c6b66a854f130a07bedf
dmytrohapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2022-09-08 21:13:37 +0300

    Update and rename task13_3.py to homework.py

---
+++ TASK_13/homework.py
@@ -0,0 +1,24 @@
+def digit(number, operator=None):
+    if operator is None:
+        return number
+    return operator(number)
+
+def zero(operator =None): return digit(0, operator)
+def one(operator=None): return digit(1, operator)
+def two(operator=None): return digit(2, operator)
+def three(operator=None): return digit(3, operator)
+def four(operator=None): return digit(4, operator)
+def five(operator=None): return digit(5, operator)
+def six(operator=None): return digit(6, operator)
+def seven(operator=None): return digit(7, operator)
+def eight(operator=None): return digit(8, operator)
+def nine(operator=None): return digit(9, operator)
+
+def plus(second_operand): 
+    return lambda first_operand: int(first_operand + second_operand)
+def minus(second_operand): 
+    return lambda first_operand: int(first_operand - second_operand)
+def times(second_operand): 
+    return lambda first_operand: int(first_operand * second_operand)
+def divided_by(second_operand): 
+    return lambda first_operand: int(first_operand / second_operand)
