76e73e0c67817368d421a1a245782ceaf939ec26
Dmytro Hapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2022-12-20 13:22:53 +0200

    Add files via upload

---
+++ TASK_27/task_2.py
@@ -0,0 +1,6 @@
+"""
+Task 2
+Using breadth-first search write an algorithm that can determine 
+the shortest path from each vertex to every other vertex. 
+This is called the all-pairs shortest path problem.
+"""
