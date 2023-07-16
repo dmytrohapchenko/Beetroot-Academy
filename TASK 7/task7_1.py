779b0979409c8101b0568f89e25dc334c422ca19
Dmytro Hapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2023-07-16 15:11:00 +0300

    Update task7_1.py

--- TASK 7/task7_1.py
+++ TASK 7/task7_1.py
@@ -1,19 +1,23 @@
+import pprint
 import string
 
 
-sentence = input()
+def main():
+    sentence = input()
+    sentence = sentence.translate(str.maketrans('', '', string.punctuation))  # видаляє пунктуацію
+    sentence = sentence.lower().split(' ')
+    key_words = []
+    l_list = {}
+    for i in sentence:
+        sentence.count(i)
+        if sentence.count(i) > 1:
+            key_words.append(i)
 
-sentence = sentence.translate(str.maketrans('', '', string.punctuation)) # видаляє пунктуацію
-sentence = sentence.lower().split(' ')
-key_words = []
-l_list = {}
+    for i in range(len(key_words)):
+        l_list[key_words[i]] = sentence.count(key_words[i])
+        
+    return l_list
 
-for i in sentence:
-    sentence.count(i)
-    if sentence.count(i)>1:
-        key_words.append(i)
 
-for i in range(len(key_words)):
-    l_list[key_words[i]] = sentence.count(key_words[i])
-
-print(l_list)
+if __name__ == '__main__':
+    pprint.pprint(main())
