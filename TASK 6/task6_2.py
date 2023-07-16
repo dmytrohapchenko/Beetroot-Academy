512d13cb62adafb9d141e87f12a9480711ab6ffd
Dmytro Hapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2023-07-16 14:55:37 +0300

    Update task6_2.py

--- TASK 6/task6_2.py
+++ TASK 6/task6_2.py
@@ -1,13 +1,14 @@
 import random
 
-numbers_1 = list()
-numbers_2 = list()
-numbers_3 = []
-for i in range(10):
-    numbers_1.append(random.randint(1,11))
-    numbers_2.append(random.randint(1,11))
-for j in range(len(numbers_1)):
-    numbers_3.append(numbers_1[j])
-    numbers_3.append(numbers_2[j])
+def main():
+    l_list1 = [random.randint(1, 11) for i in range(10)]
+    l_list2 = [random.randint(1, 11) for i in range(10)]
+    l_list3 = []
+    for i in range(len(l_list1)):
+        l_list3.append(l_list1[i])
+        l_list3.append(l_list2[i])
+    return list(set(l_list3))
 
-print(list(set(numbers_3)))
+
+if __name__ == '__main__':
+    print(main())
