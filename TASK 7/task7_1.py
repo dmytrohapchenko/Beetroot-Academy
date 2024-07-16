86b66d169556788df9f3b301427bbfc975335bb9
Dmytro Hapchenko <111082636+dmytrohapchenko@users.noreply.github.com>
2024-07-16 15:56:05 +0300

    Update task7_1.py

--- TASK 7/task7_1.py
+++ TASK 7/task7_1.py
@@ -1,23 +1,20 @@
 import pprint
-import string
 
+def main(sentence):
+    sentence = sentence.lower()
+    words = sentence.split()
+    word_count ={}
 
-def main():
-    sentence = input()
-    sentence = sentence.translate(str.maketrans('', '', string.punctuation))  # видаляє пунктуацію
-    sentence = sentence.lower().split(' ')
-    key_words = []
-    l_list = {}
-    for i in sentence:
-        sentence.count(i)
-        if sentence.count(i) > 1:
-            key_words.append(i)
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
 
-    for i in range(len(key_words)):
-        l_list[key_words[i]] = sentence.count(key_words[i])
-        
-    return l_list
+    return word_count
 
+input_sentence = input("Enter a sentence: ")
 
-if __name__ == '__main__':
-    pprint.pprint(main())
+word_counts = main(input_sentence)
+
+print(word_counts)
